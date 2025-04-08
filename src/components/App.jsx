24bbcbd001cd200929bf3928f9a07513c1a972db
SvitlanaLucky <svitlanalucky04@gmail.com>
2025-04-08 20:45:30 +0300

    App

---
+++ src/components/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Options from './Options/Options';
+import Description from './Description/Description';
+import Feedback from './Feedback/Feedback';
+import Notification from './Notification/Notification';
+
+const App = () => {
+  const [obj, setObj] = useState(() => {
+    const savedFeedback = window.localStorage.getItem('saved-feedback');
+    if (savedFeedback !== null) {
+      return JSON.parse(savedFeedback);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  });
+
+  const updateFeedback = (feedbackType) => {
+    setObj({
+      ...obj,
+      [feedbackType]: obj[feedbackType] + 1,
+    });
+  };
+
+  const resetFeedback = () => {
+    setObj({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+
+  useEffect(() => {
+    const savedFeedback = JSON.stringify(obj);
+    window.localStorage.setItem('saved-feedback', savedFeedback);
+  }, [obj]);
+
+  const totalFeedback = obj.good + obj.neutral + obj.bad;
+  const positiveFeedback = Math.round((obj.good / totalFeedback) * 100);
+
+  return (
+    <div>
+      <Description />
+      <Options
+        update={updateFeedback}
+        reset={resetFeedback}
+        total={totalFeedback}
+      />
+      <Feedback {...obj} total={totalFeedback} positive={positiveFeedback} />
+      <Notification total={totalFeedback} />
+    </div>
+  );
+};
+
+export default App;
